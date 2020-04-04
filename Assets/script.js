c71925f3dbfbed55f9118dea851b9713faddac0a
Emily Carlisle <emilydeluna96@gmail.com>
2020-04-04 17:40:56 -0500

    question screen hide

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,20 @@
-var startQuiz = document.querySelector("#start");
-var start = document.querySelector("#startScreen");
-var highscore = document.querySelector("#highscores");
-var time = document.querySelector("#time");
-var questionsScreen = document.querySelector("#questionsScreen");
-var question = document.querySelector("#question");
-var answers = document.querySelector("#answers");
-var choice1 = document.querySelector("#choice1");
-var choice2 = document.querySelector("#choice2");
-var choice3 = document.querySelector("#choice3");
-var choice4 = document.querySelector("#choice4");
-var correct = document.querySelector("#correct");
+var startQuiz = document.getElementById("start");
+var start = document.getElementById("startScreen");
+var questionsScreen = document.getElementById("questionsScreen");
+var options = document.getElementById("options")
+var choice1 = document.getElementById("choice1");
+var choice2 = document.getElementById("choice2");
+var choice3 = document.getElementById("choice3");
+var choice4 = document.getElementById("choice4");
+var correct = document.getElementById("correctAnswer");
+var highscore = document.getElementById("highscores");
+var time = document.getElementById("time");
+
+//var question = document.getElementById("question");
+//var answers = document.getElementById("answers");
+
 var currentIndex  = 0;
-var timer = 75;
+var currentTime = 75;
 var interval;
 
 
@@ -21,35 +24,29 @@ var interval;
 //    questionsScreen.style.display = 'none';
 //}
 //function () {
-//    document.querySelector('#questionsScreen').style.visibility ="hidden";
+//    document.getElementById('questionsScreen').style.visibility ="hidden";
 //}
 //when start button is clicked, I want to trigger the timer function, interval function, 
 //and the function to generate the next question
 startQuiz.addEventListener("click", function(){
-    //start.classList.add("hide");
+    startScreen.classList.add("d-none");
+    questionContainer.classList.remove("d-none");
     if (start.style.display === "none") {
         start.style.display = "block";
       } else {
         start.style.display = "none";
     }
-    //document.querySelector('#questionsScreen').classList.add("hide");
-    //questionsScreen.style.display = "block";
     generateQuestion(currentIndex);
     timer();
     enableClicks();
-})
-
-function generateQuestion(index) {
-    if(index === question.length) {
-        clearInterval(interval);
-        return 
-    }
-}
-
+});
+//document.getElementById('questionsScreen').classList.add("hide");
+    //questionsScreen.style.display = "block";
+   
 function timer() {
     interval = setInterval(function() {
         time.innerHTML = currentTime--;
-        if(answer === 'Incorrect') {
+        if(correct === 'Incorrect') {
             currentTime = currentTime - 10;
         }
         if(currentTime === -1) {
@@ -72,18 +69,53 @@ function enableClicks() {
         generateQuiz(choice4);
     })
 }
-//var startPage = document.querySelector("startpage");
-//var questionsSection = document.querySelector("questionsSection");
-//var startTimer = document.querySelector("#starttime")
+
+function generateQuestion(index) {
+    if(index === question.length) {
+        clearInterval(interval);
+        return score.innerHTML = time.innerHTML;
+    }
+    questionsScreen.innerHTML = question[index].question;
+    choice1.innerHTML = questions[index].correct.a; 
+    choice2.innerHTML = questions[index].correct.b; 
+    choice3.innerHTML = questions[index].correct.c;
+    choice4.innerHTML = questions[index].correct.d;
+}
+
+
+ //main function for the quiz
+function generateQuiz(UserChoice) {
+    var options = question[currentIndex].choice1;
+    if(UserChoice.innerHTML === correctAnswer) {
+       options.innerHTML = 'Correct'; 
+    } else {
+        options.innerHTML = 'Incorrect';
+        currentTime = currentTime - 10;
+    }
+    currentIndex++;
+    generateQuestion(currentIndex);
+}
+
+//var question [
+//    {
+//
+//        
+//    }
+//]
+
+
+//var startPage = document.getElementById("startpage");
+//var questionsSection = document.getElementById("questionsSection");
+//var startTimer = document.getElementById("starttime")
 
 //function quiz() {
-//    document.querySelector('#startPage').classList.add("hide");
-//    document.querySelector('questionsSection').classList.remove("hide");
+//    document.getElementById('startPage').classList.add("hide");
+//    document.getElementById('questionsSection').classList.remove("hide");
 //}
 //startQuiz.addEventListener("click", quiz);
 //update = setInterval("timer()", 1000); // put function here and not a string
-//    document.querySelector("#startPage").classList.add("hide");
-//    document.querySelector("#questionsSection").classList.remove("hide");
+//    document.getElementById("startPage").classList.add("hide");
+//    document.getElementById("questionsSection").classList.remove("hide");
 /*
 When start quiz button is clicked, a timer starts; 
 Questions with 4 multiple choice answers appear;
